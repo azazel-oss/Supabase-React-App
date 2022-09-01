255312843fc960d60733bded5c4f0b8e5a1a2579
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-02 04:10:58 +0530

    Add code to connect to supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import supabase from "../config/supabaseClient";
 const Home = () => {
+  console.log(supabase);
   return (
     <div className="page home">
       <h2>Home</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
